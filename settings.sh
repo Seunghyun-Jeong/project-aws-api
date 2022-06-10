382bec24cb81bfa27ec7b178c0f3c6416e8884b8
Seunghyun-Jeong <dkskakdgka@gmail.com>
2022-06-10 10:06:11 +0900

    settings.sh 수정

--- settings.sh
+++ settings.sh
@@ -1,9 +1,8 @@
 sudo apt update
 
-cd project2
-
-docker build --tag project2:0.1 .
+docker build -t project2:0.1 .
 
 docker ps -aq | xargs docker stop | xargs docker rm >& 1.log
 
-docker run --name pj2 -p 80:80 project2:0.1
+#docker run --name pj2 -p 80:80 project2:0.1
+docker run -i project2:0.1 
